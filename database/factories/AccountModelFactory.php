0b0829c0306fb632e614af53b2e822d0ca98f6d6
MinhDuong-cloo <minhduong14499@gmail.com>
2023-08-13 15:16:26 +0700

    update database

--- database/factories/AccountModelFactory.php
+++ database/factories/AccountModelFactory.php
@@ -6,7 +6,6 @@ use App\Models\StoreModel;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AccountModel>
@@ -27,8 +26,8 @@ class AccountModelFactory extends Factory
             'db_customer_id' => null,
             'db_account_name' => fake()->userName(),
             'db_account_password' => Hash::make(md5('daiminhduong@gmail.com')),
-            'db_account_token' => Str::random(99),
-            'db_account_refresh_token' => Str::random(99),
+            'db_account_token' => null,
+            'db_account_refresh_token' => null,
             'db_account_device' => '172.160.16.' . rand(1, 100),
             'db_account_status' => rand(1, 3),
             'db_account_created_at' => Carbon::now()->timestamp,
